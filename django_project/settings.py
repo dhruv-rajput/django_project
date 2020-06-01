ddfd93ee2c0b4b478ff8897540746a331351bfd1
dhruv-rajput <dhruvguzi@gmail.com>
2020-06-01 13:19:35 +0530

    Initial Commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,6 +1,7 @@
 
 
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -140,4 +141,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+django_heroku.settings(locals())
